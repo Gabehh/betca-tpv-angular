2f6e77452d8bcfcfbdc51b245d27a805379338e9
jbernal <miw@etsisi.upm.es>
2020-02-10 19:41:23 +0100

    improve css to clock & head

--- src/app/core/date.component.ts
+++ src/app/core/date.component.ts
@@ -1,15 +1,16 @@
-import {Attribute, Component, NgZone} from '@angular/core';
+import {Component} from '@angular/core';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-date',
-  template: `<a>{{date | date: format}}</a>`,
+  template: `<a>{{date | date: "medium"}}</a>`,
   styles: [`
     a {
       padding-left: 10px;
       padding-right: 10px;
     }
 
-    @media screen and (max-width: 600px) {
+    @media screen and (max-width: 700px) {
       a {
         visibility: hidden;
         display: none;
@@ -19,19 +20,11 @@ import {Attribute, Component, NgZone} from '@angular/core';
 })
 export class DateComponent {
 
-  public date: Date;
-  public format: string;
-
-  constructor(@Attribute('format') format: string, private ngZone: NgZone) {
-    this.format = format;
-    this.date = new Date();
-    this.ngZone.runOutsideAngular(() => {
-      setInterval(() => {
-        this.ngZone.run(() => {
-          this.date = new Date();
-        });
-      }, 1000);
-    });
+  date: Date;
+  constructor() {
+    timer(0, 1000).subscribe(
+      () => this.date = new Date()
+    );
   }
 
 }
