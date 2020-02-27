a70c31b22b9f00c7e1072744b5f74e76eea37255
pablo perez <pperezj@alumnos.unex.es>
2020-02-27 09:27:05 +0100

    Initial view Advanced Articles Search #12

--- src/app/app-services.ts
+++ src/app/app-services.ts
@@ -5,6 +5,7 @@ import {CashierService} from './home/shared/cashier.service';
 import {ShoppingCartService} from './home/cashier-opened/shopping-cart/shopping-cart.service';
 import {SystemService} from './home/system.service';
 import {UserService} from './home/users/user.service';
+import {AdvancedArticlesSearchService} from './home/cashier-opened/advanced-search/advanced-articles-search.service';
 
 export class AppServices {
   public static SERVICES = [
@@ -14,6 +15,7 @@ export class AppServices {
     CashierService,
     ShoppingCartService,
     SystemService,
-    UserService
+    UserService,
+    AdvancedArticlesSearchService
   ];
 }
