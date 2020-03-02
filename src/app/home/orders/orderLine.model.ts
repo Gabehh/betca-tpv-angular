55bd8dac6782a88a5883cdfe9fddef70d0597d77
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-03-02 20:04:40 +0100

    Basic Order View #3

---
+++ src/app/home/orders/orderLine.model.ts
@@ -0,0 +1,5 @@
+export interface OrderLine {
+  articleId: string;
+  requiredAmount: number;
+  finalAmount: number;
+}
--- src/app/home/orders/orders.component.ts
+++ src/app/home/orders/orders.component.ts
@@ -1,9 +1,45 @@
 import {Component} from '@angular/core';
+import {Order} from './order.model';
 
 @Component({
   templateUrl: `orders.component.html`
 })
 
 export class OrdersComponent {
+
+  order: Order;
+
+  title = 'Orders management';
+  columns = ['description', 'provider Id', 'opening Date'];
+  data: Order[];
+
+  constructor() {
+    this.order = {description: null, providerId: null, orderLines: null, openingDate: null};
+    this.data = null;
+  }
+
+  search() {
+    // TODO
+  }
+
+  resetSearch() {
+    this.order = {description: null, providerId: null, orderLines: null, openingDate: null};
+  }
+
+  create() {
+    // TODO
+  }
+
+  read(order: Order) {
+    // TODO
+  }
+
+  update(order: Order) {
+    // TODO
+  }
+
+  delete(order: Order) {
+    // TODO
+  }
 
 }
