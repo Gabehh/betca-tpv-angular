938c282925a0b3daf8928d04e58f41527cbc50ab
jesusgarce <tarraga.jesus@gmail.com>
2020-03-03 22:37:31 +0100

    Added consumed function for a voucher #20

--- src/app/home/cashier-opened/shopping-cart/vouchers/vouchers.component.ts
+++ src/app/home/cashier-opened/shopping-cart/vouchers/vouchers.component.ts
@@ -4,6 +4,7 @@ import {Voucher} from './voucher.model';
 import {VoucherService} from './voucher.service';
 import {VoucherCreationDialogComponent} from './voucher-creation-dialog.component';
 import {MatDialog} from '@angular/material';
+import {CancelYesDialogComponent} from '../../../../core/cancel-yes-dialog.component';
 
 @Component({
   templateUrl: `vouchers.component.html`
@@ -40,7 +41,15 @@ export class VouchersComponent {
     // TODO
   }
 
-  update(voucher: Voucher) {
-    // TODO
+  delete(voucher: Voucher) {
+    this.dialog.open(CancelYesDialogComponent).afterClosed().subscribe(
+      result => {
+        if (result) {
+          this.voucherService.consume(voucher).subscribe(value => {
+              this.search();
+            }
+          );
+        }
+      });
   }
 }
