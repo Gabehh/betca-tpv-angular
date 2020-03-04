cb7ed13bc1a53c10f48c4c038fbf9561f2f41c98
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-04 13:00:15 +0100

    Update ArticleFamilyView Model

--- src/app/home/cashier-opened/articles-family/articles-family-view.model.ts
+++ src/app/home/cashier-opened/articles-family/articles-family-view.model.ts
@@ -1,11 +1,5 @@
 export class ArticleFamilyView {
-  code?: string;
-  registrationDate?: string;
-  familyType: string;
-  description: string;
-  retailPrice?: string;
-  reference?: string;
-  stock?: number;
-  provider?: string;
-  size?: string;
+  id?: string;
+  FamilyType?: string;
+  reference: string;
 }
