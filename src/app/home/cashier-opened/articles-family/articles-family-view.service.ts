abf386f279194017477d1913f78416465bc30c25
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-05 20:36:54 +0100

    Mock service response

--- src/app/home/cashier-opened/articles-family/articles-family-view.service.ts
+++ src/app/home/cashier-opened/articles-family/articles-family-view.service.ts
@@ -11,10 +11,27 @@ export class ArticleFamilyViewService {
   constructor(private httpService: HttpService) {
   }
 
-  readFamilyComposite(familyDescription: string): Observable<ArticleFamilyView[]> {
-    return this.httpService
-      .param('description', familyDescription)
-      .get(AppEndpoints.ARTICLES_FAMILY + AppEndpoints.ARTICLES_FAMILY_COMPOSITE);
+  // readFamilyComposite(familyDescription: string): Observable<ArticleFamilyView[]> {
+  //   return this.httpService
+  //     .param('description', familyDescription)
+  //     .get(AppEndpoints.ARTICLES_FAMILY + AppEndpoints.ARTICLES_FAMILY_COMPOSITE);
+  // }
+
+  readFamilyComposite(familyDescription: string): any {
+    if (familyDescription === 'ROOT') {
+      return [
+        {id: '1', familyType: 'ARTICLES', reference: 'root'},
+        {id: '1', familyType: 'ARTICLES', reference: 'Zz'},
+        {id: '2', familyType: 'SIZES', reference: 'Zz Falda'},
+        {id: '2', familyType: 'SIZES', reference: 'Zz Polo'},
+        {id: '3', familyType: 'ARTICLE', reference: 'Zz Falda T2'},
+        {id: '3', familyType: 'ARTICLE', reference: 'Zz polo T2'},
+        {id: '3', familyType: 'ARTICLE', reference: 'Zz polo T4'},
+        {id: '4', familyType: 'ARTICLES', reference: 'varios'},
+        {id: '5', familyType: 'SIZES', reference: 'Zz Polo'},
+      ];
+    }
   }
+
 
 }
